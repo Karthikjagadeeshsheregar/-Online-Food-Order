1f9b2ed450709de39433e3486c9f292034f6ee6a
nagesha <nageshauppara2003@gmail.com>
2024-11-26 10:39:48 +0530

    add the food

--- Online-Food-Order/add_food_items1.php
+++ Online-Food-Order/add_food_items1.php
@@ -8,7 +8,7 @@ header('Location: managerlogin.php');
 
 
 
-$name = $conn->real_escape_string($_POST['name']);
+$name = $conn->real_escape_string($_POST['name food']);
 $price = $conn->real_escape_string($_POST['price']);
 $description = $conn->real_escape_string($_POST['description']);
 
